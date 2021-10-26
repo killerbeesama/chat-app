04764ed4b02d8c3ee33b56f6b7109d5284612ce0
killerbee <killerbeesama07@gmail.com>
2021-10-26 20:03:13 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+// Socket 
+const io = require('socket.io')(http)
+
+const users = {};
+
+io.on('connection', socket =>{
+    socket.on('new-user-joined', name =>{
+        // console.log('new user', name)
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive',{message: message, name: users[socket.
+        id]})
+    });
+
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('leave', users[socket.id])
+        delete users[socket.id];
+    });
+
+})
